644375a670e9d8325db3d405596a4f95dfd4d2c0
DmetroSK <ygpramuditha@gmail.com>
2024-06-25 10:25:43 +0530

    3.0.0

--- generate_vcards.js
+++ generate_vcards.js
@@ -2,24 +2,25 @@ const XLSX = require("xlsx");
 const fs = require("fs");
 const readlineSync = require("readline-sync");
 
-// Get input from the user
-const xlsxFileName =
-  readlineSync.question(
-    "Enter the name of the XLSX file (without extension): "
-  ) + ".xlsx";
-const categoryName = readlineSync.question("Enter the category name: ");
-const prefix = readlineSync.question("Enter the prefix: ");
-const outputVcfName =
-  readlineSync.question(
-    "Enter the name of the output VCF file (without extension): "
-  ) + ".vcf";
+try {
+  // Get input from the user
+  const xlsxFileName =
+    readlineSync.question(
+      "Enter the name of the XLSX file (without extension): "
+    ) + ".xlsx";
+  const categoryName = readlineSync.question("Enter the category name: ");
+  const prefix = readlineSync.question("Enter the prefix: ");
+  const outputVcfName =
+    readlineSync.question(
+      "Enter the name of the output VCF file (without extension): "
+    ) + ".vcf";
 
-// Read the Excel file
-const workbook = XLSX.readFile(xlsxFileName);
-const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+  // Read the Excel file
+  const workbook = XLSX.readFile(xlsxFileName);
+  const worksheet = workbook.Sheets[workbook.SheetNames[0]];
 
-// Define the vCard template
-const vcardTemplate = `BEGIN:VCARD
+  // Define the vCard template
+  const vcardTemplate = `BEGIN:VCARD
 VERSION:3.0
 FN:{firstName}
 N:{lastName};{prefix} {firstName};
@@ -28,22 +29,27 @@ CATEGORIES:${categoryName}
 END:VCARD
 `;
 
-let vcardOutput = "";
+  let vcardOutput = "";
 
-// Convert worksheet to JSON and process each row
-XLSX.utils.sheet_to_json(worksheet, { header: 1, range: 1 }).forEach((row) => {
-  const [firstName, lastName, phone] = row;
-  const vcard = vcardTemplate
-    .replace("{firstName}", firstName)
-    .replace("{lastName}", lastName)
-    .replace("{prefix}", prefix)
-    .replace("{firstName}", firstName)
-    .replace("{prefix}", prefix)
-    .replace("{phone}", phone);
-  vcardOutput += vcard + "\n";
-});
+  // Convert worksheet to JSON and process each row
+  XLSX.utils
+    .sheet_to_json(worksheet, { header: 1, range: 1 })
+    .forEach((row) => {
+      const [firstName, lastName, phone] = row;
+      const vcard = vcardTemplate
+        .replace("{firstName}", firstName)
+        .replace("{lastName}", lastName)
+        .replace("{prefix}", prefix)
+        .replace("{firstName}", firstName)
+        .replace("{prefix}", prefix)
+        .replace("{phone}", phone);
+      vcardOutput += vcard + "\n";
+    });
 
-// Write the output to a VCF file
-fs.writeFileSync(outputVcfName, vcardOutput);
+  // Write the output to a VCF file
+  fs.writeFileSync(outputVcfName, vcardOutput);
 
-console.log(`VCF file '${outputVcfName}' created successfully.`);
+  console.log(`VCF file '${outputVcfName}' created successfully.`);
+} catch (error) {
+  console.error("An error occurred:", error.message);
+}
